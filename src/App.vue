b6ca9fc2bd535ed87d41b5c27fcfe7df0c9b201c
Olzhas <okuspakov03@mail.ru>
2023-01-10 17:56:37 +0500

    secondCommit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Анкетирование</h1>
+    
+    <hr>
+    <h2>Сколько часов вы спите</h2>
+    <MyFirstPage 
+    v-bind:questions="questions"   
+    />
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyFirstPage from '@/components/MyFirstPage.vue'
 export default {
   name: 'App',
+  data(){
+    return{
+      questions: [
+        {id:1, title: 'Вы высыпаетесь?', completed:false},
+        {id:2, title: 'Сколько часов вы спите', completed:false},
+        {id:3, title: 'Как вы спите', completed:false},
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    MyFirstPage
   }
 }
 </script>
@@ -21,6 +37,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
 }
 </style>
